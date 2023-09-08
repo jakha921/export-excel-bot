1d4cf1b72076fd348f50cfca14f3898f66b11170
my <jakha921@gmail.com>
2023-09-08 11:58:46 +0500

    add error handler

--- runner.py
+++ runner.py
@@ -3,7 +3,9 @@ from backet_parser import parse_backet
 
 
 def run():
-    parse_backet()
+    output_file_path, errors = parse_backet()
+    if errors:
+        return output_file_path, errors
     path, errors = open_files()
     print('path', path)
     return path, errors
