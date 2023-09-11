ca02f0f7b066a7743b4f5517e3fca62f902cd3c0
my <jakha921@gmail.com>
2023-09-11 10:39:45 +0500

    modify error handler

--- backet_parser.py
+++ backet_parser.py
@@ -1,4 +1,5 @@
 import os
+import re
 import warnings
 from datetime import datetime
 
@@ -7,6 +8,8 @@ import json
 
 
 def parse_invoice_data(file_path):
+    # Create a list to collect error messages
+    error_messages = []
 
     # Suppress the Data Validation warning
     warnings.filterwarnings("ignore", category=UserWarning,
@@ -22,6 +25,46 @@ def parse_invoice_data(file_path):
     # Loop through all sheets
     for sheet in sheets:
         sheet = workbook[sheet]
+
+        # if B15 and C15 is empty continue iteration. Example if B15 is empty C15 must be empty too
+        if not sheet["B15"].value:
+            print("skip sheet", sheet.title)
+            continue
+
+        # Exceptions handling
+        try:
+            # Check if the sheet is empty
+            if not sheet["D4"].value:
+                raise ValueError(f"Sender name is missing")
+            if not sheet["D6"].value:
+                raise ValueError("Recipient name is missing")
+            if not sheet["D7"].value:
+                raise ValueError(f"Passport number is missing")
+            if not sheet["D8"].value:
+                raise ValueError(f"Date of birth is missing")
+            # Check if the date of birth is in the correct format and accept 08,09,1992 this format also
+            # if not isinstance(sheet["D8"].value, datetime) or not re.match(r'\d{2},\d{2},\d{4}',
+            #                                                                str(sheet["D8"].value)):
+            dob_value = str(sheet["D8"].value).split()[0].replace('-', '.').replace(',', '.')
+
+            # Change formate 1988.12.27 to 27.12.1988
+            if re.match(r'\d{4}.\d{2}.\d{2}', dob_value):
+                dob_value = datetime.strptime(dob_value, "%Y.%m.%d")
+                dob_value = dob_value.strftime("%d.%m.%Y")
+
+            if not isinstance(dob_value, datetime):
+                # Attempt to match either format using regular expressions
+                if not re.match(r'\d{2}\.\d{2}\.\d{4}$', str(dob_value)) and not re.match(r'\d{2},\d{2},\d{4}$',
+                                                                                          str(dob_value)):
+                    raise ValueError(f'Incorrect date format: {dob_value}')
+
+            if not sheet["H45"].value:
+                raise ValueError(f"Weight is missing")
+            if not sheet["D10"].value:
+                raise ValueError(f"Phone number is missing")
+        except BaseException as e:
+            print(f'Error in sheet {sheet.title}: {e}')
+            error_messages.append(f'Sheet {sheet.title}: {e}')
 
         # Extract data from specific cells
         data = {}
@@ -39,51 +82,62 @@ def parse_invoice_data(file_path):
                 dob_string = str(dob_datetime)
             data['dob'] = dob_string
         except Exception as e:
-            print('error dob', e)
-            raise f'error dob {e}'
+            # print('error dob in invoice', data['invoice_number'], e)
+            raise f'error dob in invoice {data["invoice_number"]} {e}'
 
+        # print('invoice_number', data['invoice_number'])
         data['weight'] = sheet['H45'].value
         data['phone'] = sheet['D10'].value
 
         # Extract product data
-        product_data = []
-        for row_index in range(15, 44):
-            product_name = sheet.cell(row=row_index, column=2).value
-            product_info = sheet.cell(row=row_index, column=3).value
-            product_quantity = sheet.cell(row=row_index, column=6).value
-            product_price = sheet.cell(row=row_index, column=7).value
+        try:
+            product_data = []
+            for row_index in range(15, 44):
+                product_name = sheet.cell(row=row_index, column=2).value
+                product_info = sheet.cell(row=row_index, column=3).value
+                product_quantity = sheet.cell(row=row_index, column=6).value
+                product_price = sheet.cell(row=row_index, column=7).value
 
-            # Stop parsing when an empty row is encountered
-            if not product_name:
-                break
+                # Stop parsing when an empty row is encountered
+                if not product_name:
+                    break
 
-            product = {
-                'product_name': product_name,
-                'product_info': product_info,
-                'product_quantity': product_quantity,
-                'product_price': product_price
-            }
-            product_data.append(product)
+                product = {
+                    'product_name': product_name,
+                    'product_info': product_info,
+                    'product_quantity': product_quantity,
+                    'product_price': product_price
+                }
+                product_data.append(product)
+        except Exception as e:
+            # print('error add to product in invoice', data['invoice_number'], e)
+            error_messages.append(f'error add to product in invoice {data["invoice_number"]} {e}')
 
         data['products'] = product_data
 
         # Append the data of the current sheet to the list of all_data
         all_data.append(data)
 
-    return all_data
+    return all_data, error_messages
 
 
 def parse_backet():
+    print('parse to dict')
     # Replace 'file_path.xlsx' with the path to your Excel file
-    parsed_data = parse_invoice_data('exeles\parsing_file.xlsx')
+    parsed_data, errors = parse_invoice_data('exeles\parsing_file.xlsx')
 
+    if errors:
+        return None, errors
+
+    print('parse to json')
     # Construct the output file path using os.path.join
     output_file_path = os.path.join('src', 'parsed_file.json')
 
     # Save the parsed data to a JSON file
+    print('save to json')
     with open(output_file_path, 'w', encoding='utf-8') as json_file:
         json.dump(parsed_data, json_file, ensure_ascii=False, indent=4)
 
     print(f"Parsed data has been saved to '{output_file_path}'.")
 
-    return output_file_path
+    return output_file_path, errors
